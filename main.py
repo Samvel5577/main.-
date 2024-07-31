0e95b84e2f2434562580978340c1eb31297272ae
Samvel <tradeup.education@gmail.com>
2024-07-31 12:51:50 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+number_of_tasks = 12
+print(number_of_tasks)
+number_of_hours = 1.5
+print(number_of_hours)
+course_name = 'Phyton'
+print(course_name)
+Time_for_the_task = number_of_hours / number_of_tasks
+print(number_of_hours / number_of_tasks)
+
+print("Курс:",course_name, ",", "всего задач:", number_of_tasks,",","затрачено часов:",number_of_hours,",", "среднее время",Time_for_the_task,"часов.")
+
